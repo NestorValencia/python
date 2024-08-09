32a4142b458a57b053533147eef86dda708ef8c5
NestorValencia <nestoradrielvalenciaperez@gmail.com>
2024-08-08 22:12:58 -0600

    simple calculator

---
+++ test.py
@@ -0,0 +1,6 @@
+numero1 = int(input("introduce el primer número:"))
+numero2 = int(input("introduce el segundo número:"))
+
+resultado = numero1 + numero2
+
+print("El reultado de la suma es:" ,resultado)
